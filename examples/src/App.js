dd148d059627ff6454d4a128f0ffcad6967e6cd9
Solaiman  Kmail <psokmail@gmail.com>
2018-09-01 21:53:11 +0300

    Update Examples

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,67 +1,80 @@
-import React, {Component} from 'react';
-import './App.css'
-import Transform from 'react-free-transform'
+import React from "react";
+import ReactDOM from "react-dom";
+import FreeTransform from "react-free-transform";
+import ReactJson from "react-json-view";
 
-class App extends Component {
+import "./App.css";
+import "./tr.css";
+
+class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       elements: [
         {
           id: "el-1",
           x: 100,
-          y: 100,
+          y: 50,
           scaleX: 1,
           scaleY: 1,
           width: 100,
           height: 100,
           angle: 0,
-          background: "linear-gradient(135deg, #0FF0B3 0%,#036ED9 100%)"
+          background: "linear-gradient(135deg, #0FF0B3 0%,#036ED9 100%)",
+          classPrefix: "tr",
         },
         {
           id: "el-2",
-          x: 300,
-          y: 100,
+          x: 100,
+          y: 225,
           scaleX: 1,
           scaleY: 1,
           width: 100,
           height: 100,
-          angle: 0,
-          background: "linear-gradient(135deg, #fad961 0%,#f76b1c 100%)"
+          angle: 45,
+          background: "linear-gradient(135deg, #fad961 0%,#f76b1c 100%)",
+          classPrefix: "tr",
         },
         {
           id: "el-3",
-          x: 500,
-          y: 100,
+          x: 100,
+          y: 400,
           scaleX: 1,
           scaleY: 1,
           width: 100,
           height: 100,
-          angle: 0,
-          background: "linear-gradient(135deg, #b1ea4d 0%,#459522 100%)"
+          angle: 90,
+          background: "linear-gradient(135deg, #b1ea4d 0%,#459522 100%)",
+          classPrefix: "tr",
         }
       ]
-    }
+    };
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.elements.map(element => (
-            <Transform
+        <div className="workspace">
+          {this.state.elements.map(element => (
+            <FreeTransform
               key={element.id}
-              onUpdate={(payload) => this.onUpdate(element.id, payload)}
-              {...element}>
-              <div className="element" style={{
-                width: element.width,
-                height: element.height,
-                background: element.background
-              }}>
-              </div>
-            </Transform>
-          ))
-        }
+              onUpdate={payload => this.onUpdate(element.id, payload)}
+              {...element}
+            >
+              <div
+                className="element"
+                style={{
+                  width: element.width,
+                  height: element.height,
+                  background: element.background
+                }}
+              />
+            </FreeTransform>
+          ))}
+        </div>
+        <div class="json-view">
+          <ReactJson src={this.state.elements}/>
+        </div>
       </div>
     );
   }
@@ -73,12 +86,12 @@ class App extends Component {
           return {
             ...item,
             ...payload
-          }
+          };
         }
-        return item
+        return item;
       })
-    })
+    });
   }
 }
 
-export default App;
+export default App
